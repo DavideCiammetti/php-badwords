861a62475bd6fac7f830fd32cc3a7545531f4b4c
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-08 17:24:21 +0100

    fix: aggiunta style e aggiunta codice php

--- index.php
+++ index.php
@@ -7,21 +7,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>php</h1>
-
-    <form action="show.php" method="GET">
-        <!-- input frase -->
-        <label for="word" >text1</label>
-        <input type="text" id="word" name="firstWord">
-        <!-- input parola da togliere -->
-        <label for="word-two">text2</label>
-        <input type="text" id="word-Two" name="secondWord">
-        <!-- button invia -->
-        <label for="invia">invia</label>
-        <button type="submit" id="invia"  placeholder="send">send</button>
-    </form>
+    <main>
+        <form action="show.php" method="GET">
+            <!-- input frase -->
+            <label for="word">frase</label>
+            <input type="text" id="word" name="firstWord" class="m-b">
+            <!-- input parola da togliere -->
+            <label for="word-two">parola da sostituire</label>
+            <input type="text" id="word-Two" name="secondWord" class="m-b">
+            <!-- button invia -->
+            <label for="invia">invia</label>
+            <button type="submit" id="invia"  placeholder="send">send</button>
+        </form>
+    </main>
 
 </body>
 </html>
--- show.php
+++ show.php
@@ -3,13 +3,18 @@
    $parola =  $_GET['secondWord'];
    $sost = '***';
 
+   echo 'la frase inserita è:';
+   echo "\t";
    echo $frase;
+   echo ' la parola da sostituire è:';
    echo "\t";
    echo $parola;
 
-   echo ' la frase è lunga ';
+   echo ' la frase è lunga: ';
    echo "\t";
    echo strlen($frase);
+   echo "\t";
+   echo 'la frase censurata è:';
    echo "\t";
    echo str_replace($parola, $sost, $frase);
 ?>
