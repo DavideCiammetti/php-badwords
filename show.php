4a3c96dc56c27378e76a5a354f331c4148338308
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-09 14:44:59 +0100

    fix: md.doc e aggiunta elementi html in show.php

--- show.php
+++ show.php
@@ -2,17 +2,24 @@
    $frase = $_GET['firstWord'];
    $parola =  $_GET['secondWord'];
    $sost = '***';
-
-   echo 'la frase inserita è:';
-   echo $frase . "<br>";
-   echo ' la parola da sostituire è:';
-   echo $parola. "<br>";
-   
-   echo ' la frase è lunga: ';
    // metodo per calcolare la lunghezza della stringa 
-   echo strlen($frase). "<br>";
-
-   echo 'la frase censurata è:';
+   $num = strlen($frase);
    // metodo per sostituire una parola con un altra stringa 
-   echo str_replace($parola, $sost, $frase);
+   $fraseCenzurata =  str_replace($parola, $sost, $frase);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>bad words</title>
+</head>
+<body>
+   <p>la frase inserita è: <?php echo $frase ?></p> <br>
+   <p> la parola da sostituire è: <?php echo $parola?></p> <br>
+   <p> la frase è lunga: <?php echo $num?></p> <br>
+   <p>la frase censurata è: <?php echo $fraseCenzurata?></p>
+</body>
+</html>
